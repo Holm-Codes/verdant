a614bd76ca56143ed988e68891ffb9f207f16654
dittethorgaard <eaa23dith@students.eaaa.dk>
2025-04-08 13:45:54 +0200

    send

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,8 +3,25 @@ import mystyle from "./Footer.module.css";
 export default function Footer() {
 
     return (
-        <footer className={mystyle.footer}>
-            &copy; 2025 Verdant
+        <footer>
+            <div className={mystyle.bund1}>
+                <div className={mystyle.boks1}>
+                    <h3>Kontakt</h3>
+                    <p>Sønderhøj 30, 8260 Viby J</p>
+                    <p>Info@Verdant.dk</p>
+                    <p>+45 12 34 56 78</p>
+                </div>
+                <div className={mystyle.boks2}>
+                    <h3>Info</h3>
+                    <p>CVR: 123456</p>
+                    <p>Cookies- og privatpolitik</p>
+                    <p>Persondatapolitik</p>
+                </div>
+            </div>
+
+            <div className={mystyle.bund2}>
+                &copy; 2025 Verdant
+            </div>
         </footer>
     )
 }
