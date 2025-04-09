748d3c05eb7478b485da250054966cc29e8cf661
dittethorgaard <eaa23dith@students.eaaa.dk>
2025-04-09 14:35:27 +0200

    send

--- src/components/KontaktForm.jsx
+++ src/components/KontaktForm.jsx
@@ -1,74 +1,27 @@
-import styles from './KontaktForm.module.css';
+import style from './KontaktForm.module.css';
 
-const ContactForm = () => {
-  const [isChecked, setIsChecked] = useState(false);
-
-  const handleCheckboxChange = () => {
-    setIsChecked(!isChecked);
-  };
-
+export default function KontaktForm(){
   return (
-    <div className={styles.contactFormContainer}>
-      <div className={styles.contactTextContainer}>
-        <h2 className={styles.contactFormTitle}>KONTAKT OS NU</h2>
-        <p className={styles.contactFormDescription}>
-          Kontakt os nu og lad os hjælpe dig med at skabe en bæredygtig hjemmeside, der styrker din forretning.
-        </p>
+    <div className={style.KontaktForm}>
+      <div className={style.textForm}>
+        <h2>KONTAKT OS NU</h2>
+        <p>Kontakt os nu og lad os hjælpe dig med at skabe en bæredygtig hjemmeside, der styrker din forretning.</p>
       </div>
 
-      <div className={styles.formWrapper}>
-        <form>
-          <div className={styles.formGroup}>
-            <label htmlFor="fuldeNavn" className={styles.formLabel}>
-              Fulde navn:
-            </label>
-            <input
-              type="text"
-              id="fuldeNavn"
-              name="fuldeNavn"
-              className={styles.formInput}
-            />
-          </div>
-          <div className={styles.formGroup}>
-            <label htmlFor="firmaNavn" className={styles.formLabel}>
-              Firma navn:
-            </label>
-            <input
-              type="text"
-              id="firmaNavn"
-              name="firmaNavn"
-              className={styles.formInput}
-            />
-          </div>
-          <div className={styles.formGroup}>
-            <label htmlFor="besked" className={styles.formLabel}>
-              Kort om hvad vi kan hjælpe med:
-            </label>
-            <textarea
-              id="besked"
-              name="besked"
-              className={styles.formTextarea}
-            ></textarea>
-          </div>
+      <div className={style.lilleForm}>
+        <label htmlFor="fuldeNavn">Fulde navn:</label>
+          <input type="text" id="fuldeNavn" className={style.indhold} />
 
-          <div className={styles.formGroup}>
-            <label className={styles.formCheckboxLabel}>
-              <input
-                type="checkbox"
-                checked={isChecked}
-                onChange={handleCheckboxChange}
-                className={styles.formCheckboxInput}
-              />
-              Jeg accepterer Vertant's vilkår og betingelser. Jeg er klar over, at min kontaktinformation vil blive brugt til at kontakte mig om mit forespørgsel.
-            </label>
-          </div>
-          <button type="submit" className={styles.formSubmitButton}>
-            Send
-          </button>
-        </form>
+        <label htmlFor="virksomhed">Virksomhed:</label>
+          <input type="text" id="virksomhed" className={style.indhold} />
+
+        <label htmlFor="problem">Kort om hvad vi kan hjælpe med:</label>
+          <textarea id="problem" className={style.textArea}></textarea>
+        
+        <div className={style.sendKnap}>
+          <button>Send</button>
+        </div>
       </div>
     </div>
-  );
-};
-
-export default KontaktForm;
+    )
+}
