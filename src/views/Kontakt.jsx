b94a7a57b6adb81e0eb4d2189eb618d581591306
dittethorgaard <eaa23dith@students.eaaa.dk>
2025-04-10 17:58:10 +0200

    Kommentar på Kontakt

--- src/views/Kontakt.jsx
+++ src/views/Kontakt.jsx
@@ -1,10 +1,10 @@
-import mystyle from "./Kontakt.module.css";
+import mystyle from "./Kontakt.module.css"; // Dette importerer et CSS-modul, så du kan bruge classnames direkte som 'style.xxx'
 
-export default function Kontakt(){
-  function handleSubmit(event) {
-    event.preventDefault();
+export default function Kontakt(){ 
+  function handleSubmit(event) { {/* Denne funktion kører, når brugeren trykker på "Kontakt"-knappen. */}
+    event.preventDefault(); {/* Denne forhindrer, at siden genindlæses. */}
     alert("Tak fordi du valgt os, vi vender tilbage til dig så hurtigst muligt, normalt indenfor 2-3 arbejdesdage :)");
-  }
+  } {/* Der vises en alert med vores takke-besked. */}
 
     return (
         <div className={mystyle.container}>
@@ -15,11 +15,11 @@ export default function Kontakt(){
         Vi ser frem til at høre fra dig!
       </p>
 
-      <form onSubmit={handleSubmit} className={mystyle.formContainer}>
+      <form onSubmit={handleSubmit} className={mystyle.formContainer}> {/* Formular, der kalder handleSubmit når den sendes */}
         <h3 className={mystyle.formTitle}>KONTAKT FORMULAR</h3>
       
         <label htmlFor="fuldeNavn">Fulde navn:</label>
-        <input type="text" id="fuldeNavn" required className={mystyle.input} />
+        <input type="text" id="fuldeNavn" required className={mystyle.input} /> {/* required betyder, at feltet skal udfyldes før man kan sende */}
 
         <label htmlFor="virksomhed">Virksomhed:</label>
         <input type="text" id="virksomhed" required className={mystyle.input} />
@@ -33,7 +33,7 @@ export default function Kontakt(){
         <label htmlFor="problem">Kort om jeres problem:</label>
         <textarea id="problem" required className={mystyle.textarea}></textarea>
 
-        <button className={mystyle.formContainerButton}>
+        <button className={mystyle.formContainerButton}> {/*  Kontakt-knap i midten – den sender formularen (og udløser alert) */}
           Kontakt
         </button>
       </form>
